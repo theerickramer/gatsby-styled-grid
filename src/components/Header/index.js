cb0b314939bfc2a5240c121c1ad02d0dacf1e0a6
Eric Kramer <eric.kramer@showtime.net>
2018-01-17 16:33:02 -0500

    header less styles, more grid-area!

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,18 +4,12 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      gridArea: 'header',
+      background: 'rebeccapurple'
     }}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+    <div>
+      <h1>
         <Link
           to="/"
           style={{
@@ -23,7 +17,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+        Header
         </Link>
       </h1>
     </div>
