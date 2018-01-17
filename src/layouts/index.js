3d412251ceb012cbb7931b0a2e9fca4dd6466c19
Eric Kramer <eric.kramer@showtime.net>
2018-01-17 16:33:22 -0500

    Template grid

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,54 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import styled from 'styled-components';
 
-import Header from '../components/Header'
-import './index.css'
+import Header from '../components/Header';
+import './index.css';
+
+const Template = styled.div`
+  display: grid;
+  grid-template:
+    'header header header' 100px
+    '. main .' auto
+    'footer footer footer' 100px
+    / 1fr 90% 1fr;
+    width: 100%;
+    height: 100%;
+`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="What Gatsby?"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Template>
+      <Header />
+      <div
+        style={{
+          gridArea: 'main',
+          background: 'yellow'
+        }}>
+        <h1>Main</h1>
+        {children()}
+      </div>
+      <div
+        style={{
+          gridArea: 'footer',
+          background: 'yellowgreen'
+        }}>
+       <h1>Footer</h1>
+      </div>
+    </Template>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
