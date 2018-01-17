5b4ea36192e1ee4b82c5d3fb1c1f4a2b000b14dc
Eric Kramer <eric.kramer@showtime.net>
2018-01-17 16:33:33 -0500

    woop woop!

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Woop Woop</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
